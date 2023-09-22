09e844e7f3320abd2e6e757f5c8e92417a056440
Jack Chu <chuchuanbao@gmail.com>
2023-09-22 16:57:45 +0800

    feat(init): 优化注释

--- lib/src/main/java/io/github/godfather1103/annotations/MapperCodeGenerator.java
+++ lib/src/main/java/io/github/godfather1103/annotations/MapperCodeGenerator.java
@@ -12,6 +12,8 @@ package io.github.godfather1103.annotations;
  * @since 1.0
  */
 
+import org.mapstruct.Mapper;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -20,9 +22,31 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 public @interface MapperCodeGenerator {
+
+    /**
+     * 对应的Mapper类的包名，为空时将跟被注解的类同package<BR/>
+     *
+     * @return 结果
+     * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+     * @date 创建时间：2023/9/22 16:53
+     */
     String mapperPackageName() default "";
 
+    /**
+     * 对应的Mapper类的类名，为空时为“被注解的类名+EntityConvert”<BR/>
+     *
+     * @return 结果
+     * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+     * @date 创建时间：2023/9/22 16:53
+     */
     String mapperClassName() default "";
 
+    /**
+     * {@link Mapper#componentModel()}<BR>
+     *
+     * @return 结果
+     * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+     * @date 创建时间：2023/9/22 16:56
+     */
     String componentModel() default "default";
 }
