1b725c57dabe5ac0249d48a396ca5dd0f70fad9b
Jack Chu <chuchuanbao@gmail.com>
2023-09-22 17:03:39 +0800

    feat(init): 优化注释

--- lib/src/main/java/io/github/godfather1103/annotations/EntityMapping.java
+++ lib/src/main/java/io/github/godfather1103/annotations/EntityMapping.java
@@ -18,9 +18,31 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.CLASS)
 @Repeatable(EntityMappings.class)
 public @interface EntityMapping {
+
+    /**
+     * 转换的目标类<BR>
+     *
+     * @return 结果
+     * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+     * @date 创建时间：2023/9/22 17:01
+     */
     String targetEntityClass();
 
+    /**
+     * 转换的方法名<BR>
+     *
+     * @return 结果
+     * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+     * @date 创建时间：2023/9/22 17:02
+     */
     String methodName();
 
+    /**
+     * {@link Mapping}<BR>
+     *
+     * @return 结果
+     * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+     * @date 创建时间：2023/9/22 17:02
+     */
     Mapping[] mappings() default {};
 }
